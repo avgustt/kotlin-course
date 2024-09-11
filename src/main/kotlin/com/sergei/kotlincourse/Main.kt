2a66d65d31db5a3280c790dbe9864954760eaa4f
Sergei Sevostianov <avgusts14@gmail.com>
2024-09-11 19:47:06 +0300

    Kotlin files

--- src/main/kotlin/com/sergei/kotlincourse/Main.kt
+++ src/main/kotlin/com/sergei/kotlincourse/Main.kt
@@ -1,4 +1,4 @@
-package com.sergei
+package com.sergei.com.sergei.kotlincourse
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
