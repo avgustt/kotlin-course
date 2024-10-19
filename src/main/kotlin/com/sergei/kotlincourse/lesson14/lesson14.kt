4692cd21806de0cbe53528424aa5609177df974b
Sergei Sevostianov <avgusts14@gmail.com>
2024-10-19 17:16:21 +0100

    Kotlin files

--- src/main/kotlin/com/sergei/kotlincourse/lesson14/lesson14.kt
+++ src/main/kotlin/com/sergei/kotlincourse/lesson14/lesson14.kt
@@ -2,4 +2,74 @@ package com.sergei.com.sergei.kotlincourse.com.sergei.kotlincourse.lesson14
 
 fun main () {
 
+
+    val fruitsPrices = mapOf("apple" to 2.99, "banana" to 1.99, "cherry" to 3.99)
+
+    for (entry in fruitsPrices.entries) {
+        println("Key: ${entry.key}, Value: ${entry.value}")
+    }
+
+    for ((fruit, price) in fruitsPrices) {
+        println("Key: $fruit, Value: $price")
+    }
+//набор получение данных из словаря
+    val entries = fruitsPrices.entries
+    val keys = fruitsPrices.keys
+    val values = fruitsPrices.values
+
+ //методы получения элементов
+
+    val priceOfPearOrElse = fruitsPrices.getOrElse("pear") { throw IllegalArgumentException() }
+    val priceOfApple = fruitsPrices.getValue("apple")
+    val priceOfPearOrDefault = fruitsPrices.getOrDefault("pear", 0.0)
+
+
+    val fruitsWithoutBanana = fruitsPrices - "banana"
+    val fruitsWithPineapple = fruitsPrices + ("pineapple" to 3.5)
+    val mutableFruits = fruitsPrices.toMutableMap()
+
+    mutableFruits["kiwi"] = 2.75 // Добавление элемента
+    mutableFruits.putAll(mapOf("lime" to 1.1, "avocado" to 1.9))
+    mutableFruits.remove("apple") // Удаление элемента
+    mutableFruits.clear()
+
+    val containsApple = fruitsPrices.containsKey("apple")
+    val containsValue1_5 = fruitsPrices.containsValue(1.5)
+    val isEmpty = fruitsPrices.isEmpty()
+    val isNotEmpty = fruitsPrices.isNotEmpty()
+    val areAllFruitsExpensive = fruitsPrices.all { it.value > 1.0 }
+    val isAnyFruitCheap = fruitsPrices.any { it.value < 1.0 }
+
+    val filteredByPrice = fruitsPrices.filter { it.value > 1.0 }
+    val filteredByKeys = fruitsPrices.filterKeys { it.startsWith("a") }
+    val filteredByValues = fruitsPrices.filterValues { it < 2.0 }
+    val filteredNotApple = fruitsPrices.filterNot { it.key == "apple" }
+    val countExpensiveFruits = fruitsPrices.count { it.value > 1.5 }
+
+    //преобразования
+
+    val increasedPrices = fruitsPrices.mapValues { it.value * 1.1 }
+    val fruitNamesUppercase = fruitsPrices.mapKeys { it.key.uppercase() }
+    val fruitsList = fruitsPrices.map { "${it.key} costs ${it.value}" }
+
+    val toMap = mutableFruits.toMap() //создает не изменяемый
+    val toMutableMap = fruitsPrices.toMutableMap() //создает изменяемый
+
+    fruitsPrices.forEach { (fruit, price) ->
+        println("$fruit costs $price")
+    }
+    fruitsPrices.forEach {
+        println("${it.key} costs ${it.value}")
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
