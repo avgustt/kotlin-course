e35c91064b2f31fcd1b044f1ae6e8412b67c4ee8
Sergei Sevostianov <avgusts14@gmail.com>
2024-11-09 14:41:29 +0000

    Kotlin files

---
+++ src/main/kotlin/com/sergei/kotlincourse/lesson19/Student.kt
@@ -0,0 +1,9 @@
+package com.sergei.kotlincourse.lesson19
+
+class Student(name: String, age: Int, val studentId: Int) : Person(name, age) {
+
+    // Публичный метод
+    fun study() {
+        // Используем защищённый метод getName() из родительского класса
+        println("${getName()} учится.")
+    }
