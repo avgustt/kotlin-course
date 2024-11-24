e7d4c75261e5a83486f91a076ae8668cca4ae4f2
Sergei Sevostianov <avgusts14@gmail.com>
2024-11-24 12:25:55 +0000

    Kotlin files

--- src/main/kotlin/com/sergei/kotlincourse/lesson24/homework/MyException.kt
+++ src/main/kotlin/com/sergei/kotlincourse/lesson24/homework/MyException.kt
@@ -1,4 +1,13 @@
 package com.sergei.kotlincourse.lesson24.homework
 
-class MyException(cause: ArrayIndexOutOfBoundsException): RuntimeException("Сработало исключение ArrayIndexOutOfBoundsException", cause) {
+class MyException(cause: ArrayIndexOutOfBoundsException): RuntimeException(cause)
+
+fun main() {
+    try {
+        val array = arrayOf(1, 2, 3)
+        println(array[5])
+    } catch (e: ArrayIndexOutOfBoundsException) {
+        println("Перехвачено ArrayIndexOutOfBoundsException: ${e.message}")
+        throw MyException(e)
+    }
 }
