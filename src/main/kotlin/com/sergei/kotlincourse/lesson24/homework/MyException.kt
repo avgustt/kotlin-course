8459d73b32d8afdfd4e114d49de5860421dbc4db
Sergei Sevostianov <avgusts14@gmail.com>
2024-11-24 12:46:56 +0000

    Kotlin files

--- src/main/kotlin/com/sergei/kotlincourse/lesson24/homework/MyException.kt
+++ src/main/kotlin/com/sergei/kotlincourse/lesson24/homework/MyException.kt
@@ -4,7 +4,7 @@ package com.sergei.kotlincourse.lesson24.homework
 //Напиши код, который спровоцирует выброс ArrayIndexOutOfBoundsException, перехвати его с помощью try-catch и в
 //блоке catch выведи сообщение в консоль и сделай выброс своего типа исключения.
 
-class MyException(cause: ArrayIndexOutOfBoundsException): RuntimeException(cause)
+class MyException(cause: ArrayIndexOutOfBoundsException): RuntimeException("это сообщение которое я сам сюда добавил и оно распечатается красным цветом, когда исключение сработает. Еще не разобрался зачем это надо, но наверное что бы тут был какой-то умный код, который позволяет получать зарплату", cause)
 
 fun main() {
     try {
