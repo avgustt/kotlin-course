81b8f12f8b805120bf67796fce98c6d78cd27a97
Sergei Sevostianov <avgusts14@gmail.com>
2024-11-24 12:41:49 +0000

    Kotlin files

--- src/main/kotlin/com/sergei/kotlincourse/lesson24/homework/MyException.kt
+++ src/main/kotlin/com/sergei/kotlincourse/lesson24/homework/MyException.kt
@@ -1,4 +1,8 @@
 package com.sergei.kotlincourse.lesson24.homework
+//Задание 5. Создай свой тип исключения в отдельном файле, наследуемый от RuntimeException и принимающий
+//ArrayIndexOutOfBoundsException тип в качестве аргумента.
+//Напиши код, который спровоцирует выброс ArrayIndexOutOfBoundsException, перехвати его с помощью try-catch и в
+//блоке catch выведи сообщение в консоль и сделай выброс своего типа исключения.
 
 class MyException(cause: ArrayIndexOutOfBoundsException): RuntimeException(cause)
 
