9fa013a88d1ab0d74002e9d8b7c1abb0ce59d04e
Sergei Sevostianov <avgusts14@gmail.com>
2024-11-30 19:49:21 +0000

    Kotlin files

--- src/main/kotlin/com/sergei/kotlincourse/lesson25/homework/Task5.kt
+++ src/main/kotlin/com/sergei/kotlincourse/lesson25/homework/Task5.kt
@@ -1,5 +1,8 @@
 package com.sergei.kotlincourse.lesson25.homework
 
+import com.sergei.kotlincourse.lesson13.list
+
+
 //Задание 5. Создай функцию расширение списка чисел, которая будет возвращать список уникальных чисел (без дубликатов).
 //Создай аналогичную анонимную функцию.
 //Создай аналогичное лямбда выражение с указанием типа.
@@ -7,12 +10,36 @@ package com.sergei.kotlincourse.lesson25.homework
 
 
 fun main() {
-    val list = listOf<Int>(1, 2, 7, 9, 8, 2, 4, 4, 9, 9)
+    val list0 = listOf<Int>(1, 2, 7, 9, 8, 2, 4, 4, 9, 9)
 
-    println(forFive(list))
+    val list1 = listOf<Int>()
+
+    println(forFive3(list0))//проверка, что лямбда выражение работает правильно
+    println(forFive3(list1))//пустой лист
 }
 
+//обычная функция
 fun forFive(list: List<Int>): List<Int> {
     require(list.isNotEmpty()) {"Список не должен быть пустым"}
     return list.toSet().toList()
 }
+
+//аналогичная анонимная функция
+val forFive1 = fun (list: List<Int>): List<Int> {
+    require(list.isNotEmpty()) {"Список не должен быть пустым"}
+    return list.toSet().toList()
+}
+
+//лямбда выражение с указанием типа
+val forFive2: (List<Int>) -> List<Int> = {list ->
+    require(list.isNotEmpty()) {"Список не должен быть пустым"}
+    list.toSet().toList()
+}
+
+//лямбда выражение без указания типа
+val forFive3 = {list: List<Int> ->
+    require(list.isNotEmpty()) {"Список не должен быть пустым"}
+    list.toSet().toList()
+}
+
+
