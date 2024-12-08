c1112978b4e46251f34521f7cf43de5e9a483c9f
Sergei Sevostianov <avgusts14@gmail.com>
2024-12-08 15:13:58 +0000

    Kotlin files

--- src/main/kotlin/com/sergei/kotlincourse/lesson27/homework/main.kt
+++ src/main/kotlin/com/sergei/kotlincourse/lesson27/homework/main.kt
@@ -84,6 +84,45 @@ val employee1 = with(persona) {
     }
 }
 
+//Задание 5: Использование run для преобразования и инициализации Employee
+//Создайте объект класса Person. Используйте функцию run, чтобы инициализировать объект Employee,
+//добавить ему email и department с помощью apply, вернуть инициализированный объект.
+
+val personaNext = Person(
+    name = "Innokentii",
+    age = 50,
+).run {
+    Employee(
+    name = this.name,
+    age = this.age,
+    position = "Полковник",
+    ).apply {
+        email = "thees@with"
+        department = "IT"
+    }
+}
+
+//Задание 6: Использование let для безопасного преобразования Person в Employee
+//Создайте функцию, которая принимает объект класса Person?, который может быть null.
+//В функции используйте функцию let для создания Employee, только если объект Person не null.
+//В противном случае возвращается null
+
+
+
+fun functionFor6(obj: Person?): Employee?{
+   return obj?.let {
+        Employee(
+            name = it.name,
+            age = it.age,
+            position = "Примарх"
+            ).apply {
+            email = it.email
+            department = "IX легион"
+        }
+    }
+}
+
+
 fun main() {
 
 //реализация 1 задания:
@@ -94,6 +133,23 @@ fun main() {
 functiomFor3(persona)
 
 //проверяю создание 4 задания:
-println("Employee: ${employee1.name}, ${employee1.age}, ${employee1.email}, ${employee1.department}")
+println("${employee1.name}, ${employee1.age}, ${employee1.email}, ${employee1.department}, ${employee1.position}")
 
+//проверяю создание 5 задания:
+println("${personaNext.name}, ${personaNext.age}, ${personaNext.email}, ${personaNext.department}, ${personaNext.position}")
+
+//проверяю создание 6 задания:
+
+val otraPersona = Person(
+    name = "Сангвиний",
+    age = 300
+).apply {
+    email = "дворец@императора"
+}
+
+val employee6 = functionFor6(otraPersona)
+println("${employee6?.name}, ${employee6?.age}, ${employee6?.email}, ${employee6?.department}, ${employee6?.position}")
+
+val nullPerson: Person? = null
+println(functionFor6(nullPerson))
 }
