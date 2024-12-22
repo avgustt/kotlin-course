c6fcefcb5965ae6e3c0a12a64801400fa9856dd4
Sergei Sevostianov <avgusts14@gmail.com>
2024-12-22 19:18:41 +0000

    Kotlin files

--- src/test/kotlin/com.sergei.kotlincourse.lesson31.homework/Main.kt
+++ src/test/kotlin/com.sergei.kotlincourse.lesson31.homework/Main.kt
@@ -1,16 +1,6 @@
 package com.sergei.kotlincourse.lesson31.homework
 
-//2.1. Тесты для addCereal
 
 
 
-fun addCereal(cereal: Cereal, amount: Float): Float {
-    require(amount >= 0) {
-        "Количество крупы не может быть отрицательным"
-    }
-    checkStorageCapacity(cereal)
-    val currentAmount = storage.getOrDefault(cereal, 0f)
-    val amountForAdding = minOf(getSpace(cereal), amount)
-    storage[cereal] = currentAmount + amountForAdding
-    return amount - amountForAdding
-}
+
